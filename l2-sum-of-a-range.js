27d2d21125742bd225ca0108621534ae3012c080
Robert Hoekstra <robert.hoekstra@hva.nl>
2019-04-25 15:09:37 +0200

    Update l2-sum-of-a-range.js

--- l2-sum-of-a-range.js
+++ l2-sum-of-a-range.js
@@ -1,1 +1,22 @@
+function range(start, end){
+    let listOfNumbers = [start];
+        if (start <= end){
+            for (var i in (end - start)){
+                listOfNumbers.push(start+i)
+                return listOfNumbers;
 
+        }
+    // optellen als start kleiner is dan end
+        } else {
+            for (var i in (start - end)){
+                listOfNumbers.push(start-i)
+                return listOfNumbers;
+    // aftellen als start kleiner is dan end
+    }
+
+}
+    listOfNumbers.push(end);
+    return listOfNumbers;
+}
+
+console.log(range(1, 10));
