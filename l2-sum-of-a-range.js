c9814900d7034d55f92cd35e4731e6a3ec758191
Robert Hoekstra <robert.hoekstra@hva.nl>
2019-04-26 11:09:26 +0200

    sum of a range

--- l2-sum-of-a-range.js
+++ l2-sum-of-a-range.js
@@ -11,3 +11,15 @@ function range(start, end){
     return listOfNumbers;
 }
 console.log(range(1, 10));
+console.log(range(10, 1));
+
+function sum (listOfNumbers){
+    let totalOfList = 0;
+    for (var i = listOfNumbers[0]; i >= listOfNumbers[-1]; i++){
+        totalOfList += listOfNumbers[i];
+        return totalOfList;
+    }
+    return totalOfList;
+}
+
+console.log(sum(range(1, 10)));
